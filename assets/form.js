5d50ad127b4532deb96d687e0d13f184d2e5392d
jocephuss <rowlandjosiah14@gmail.com>
2024-05-18 00:36:52 -0400

    First code commit to github

---
+++ assets/form.js
@@ -0,0 +1,2 @@
+//console.log('Synced3');
+//
---
+++ assets/logic.js
@@ -0,0 +1,5 @@
+// console.log('Synced');
+// logical oporaters are used to combine or manipulate boolean values
+//  (true or false) 
+// and = &&, or = ||, not = !.
+
