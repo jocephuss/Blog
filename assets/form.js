6ea14a4859407748e5458ab3f38555d437d4353a
jocephuss <rowlandjosiah14@gmail.com>
2024-05-18 01:21:20 -0400

    Added JS for first page

--- assets/form.js
+++ assets/form.js
@@ -1,2 +1,17 @@
 //console.log('Synced3');
 //
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+
+let mode = 'dark';
+
+themeSwitcher.addEventListener('click', function () {
+    if (mode === 'dark') {
+        mode = 'light';
+        container.setAttribute('class', 'light');
+    }
+    else {
+        mode = 'dark';
+        container.setAttribute('class', 'dark');
+    }
+});
