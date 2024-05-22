f1918207e872cd8e8994b273a8bd47080ce62d9f
jocephuss <rowlandjosiah14@gmail.com>
2024-05-22 01:01:04 -0400

    removed code

--- assets/blog.js
+++ assets/blog.js
@@ -17,51 +17,6 @@ themeSwitcher.addEventListener('click', function () {
     }
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var formDataString = localStorage.getItem('formData');
-//     var formData = JSON.parse(formDataString);
-
-//     if (formData) {
-//         document.getElementById('displayUsername').textContent = formData.username;
-//         document.getElementById('displayTitle').textContent = formData.title;
-//         document.getElementById('displayContent').textContent = formData.content;
-//     }
-// });  this worked but not how i wanted, needed a pre made div 
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var formData = JSON.parse(localStorage.getItem('formData'));
-
-//     if (formData) {
-//         var blogPostDiv = document.createElement('div');
-//         blogPostDiv.classList.add('blog-post');
-
-//         var usernamePara = document.createElement('p');
-//         usernamePara.textContent = 'Author: ' + formData.username;
-
-//         var titlePara = document.createElement('h2');
-//         titlePara.textContent = 'Title:' + formData.title;
-
-//         var contentPara = document.createElement('p');
-//         contentPara.textContent = formData.content;
-
-//         blogPostDiv.appendChild(usernamePara);
-//         blogPostDiv.appendChild(titlePara);
-//         blogPostDiv.appendChild(contentPara);
-        
-//         // Append the div to the body or another location
-//         const mainElement = document.querySelector('main')
-//         mainElement.appendChild(blogPostDiv);
-         
-//     } else {
-//         // if no data is not found in localStorage
-//         console.log('No blog post data found.');
-
-//     }
-// });
-
-
-
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     var blogPosts = JSON.parse(localStorage.getItem('blogPosts'));
